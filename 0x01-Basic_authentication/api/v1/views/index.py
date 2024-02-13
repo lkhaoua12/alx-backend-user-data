d2374d693bfb528266a896ebabb0742b2e4b3b74
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-02-13 21:19:56 +0100

    restructure repository

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> str:
     """ GET /api/v1/status return abort 401"""
     return abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden """
+    return abort(403)
