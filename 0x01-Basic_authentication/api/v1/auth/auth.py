e2f9e639907bc80c69616541e9245c67c3e259bc
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-02-14 19:51:23 +0100

    check if path require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,19 +4,28 @@ from flask import request
 from typing import List, TypeVar
 
 
+
 class Auth():
     """ base class for auth system """
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ check if the url require auth """
-        return False
+        """Check if the URL requires authentication."""
+        if path is None:
+            return True
+        if not excluded_paths or not path.strip():
+            return True
+        
+        for excluded_path in excluded_paths:
+            if path == excluded_path or path + '/' == excluded_path:
+                return False
+        
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """ check the request header """
         return None
 
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ check the current user """
         return None
-
-    
