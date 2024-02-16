d5aada625270eac82db0a1be424cee3e29470118
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2024-02-16 23:48:47 +0100

    added b64 encoding

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
-""" the template for all authentication system implemented """
+"""Authentication module.
+"""
 from flask import request
 from typing import List, TypeVar
+import fnmatch
 
 
-class Auth():
-    """ base class for auth system """
+class Auth:
+    """Authentication class.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Check if the URL requires authentication."""
+        """ Method to check if auth is required.
+        """
         if path is None:
             return True
-        if not excluded_paths or not path.strip():
+
+        if excluded_paths is None or not excluded_paths:
             return True
 
         for excluded_path in excluded_paths:
-            if path == excluded_path or path + '/' == excluded_path:
+            if fnmatch.fnmatch(path, excluded_path):
                 return False
 
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ check the request header """
+        """ Method to get authorization header.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ check the current user """
+        """ Method to get user from request.
+        """
         return None
